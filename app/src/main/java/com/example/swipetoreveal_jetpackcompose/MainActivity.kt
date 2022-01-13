303bf31aeaeadbbf423a821c7f44f09aca28e44c
mohamedtamer0 <mt4452242@gmail.com>
2022-01-13 05:34:42 +0200

    Update

--- app/src/main/java/com/example/swipetoreveal_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/swipetoreveal_jetpackcompose/MainActivity.kt
@@ -5,24 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.swipetoreveal_jetpackcompose.ui.theme.LightGray100
 import com.example.swipetoreveal_jetpackcompose.ui.theme.Purple200
 import com.example.swipetoreveal_jetpackcompose.ui.theme.Purple500
 import com.example.swipetoreveal_jetpackcompose.ui.theme.SwipeToRevealjetPackComposeTheme
+import kotlin.math.roundToInt
+
 
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
@@ -53,12 +63,15 @@ fun SwipeCompose() {
     )
 
     val squareSize = 150.dp
-    val swipAbleState = rememberSwipeableState(initialValue = 0)
+    val swipeAbleState = rememberSwipeableState(initialValue = 0)
     val sizePx = with(LocalDensity.current) { squareSize.toPx() }
     val anchors = mapOf(0f to 0, sizePx to 1)
 
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -83,36 +96,115 @@ fun SwipeCompose() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(15.dp))
-                .background(Color.LightGray)
-                .swipeable(
-                    state = swipAbleState,
-                    anchors = anchors,
-                    thresholds = { _, _ ->
-                        FractionalThreshold(0.3f)
-                    },
-                    orientation = Orientation.Horizontal
-                )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(LightGray100)
+                    .swipeable(
+                        state = swipeAbleState,
+                        anchors = anchors,
+                        thresholds = { _, _ ->
+                            FractionalThreshold(0.3f)
+                        },
+                        orientation = Orientation.Horizontal
+                    )
             ) {
-                Column() {
-                    
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    IconButton(
+                        onClick = {
+                            bgColor = Color.Green
+                        },
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(CircleShape)
+                            .background(Color.LightGray)
+                    ) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Edit",
+                            tint = Color.Green
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    IconButton(
+                        onClick = {
+                            bgColor = Color.Red
+                        },
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(CircleShape)
+                            .background(Color.LightGray)
+                    ) {
+                        Icon(
+                            Icons.Filled.Delete,
+                            contentDescription = "Delete",
+                            tint = Color.Red
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .offset {
+                            IntOffset(
+                                swipeAbleState.offset.value.roundToInt(), 0
+                            )
+                        }
+                        .clip(RoundedCornerShape(15.dp))
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .fillMaxHeight()
+                        .background(color.value)
+                        .align(Alignment.CenterStart)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.image),
+                                contentDescription = "Circle Image",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(70.dp)
+                                    .clip(CircleShape)
+                            )
+
+                            Spacer(modifier = Modifier.padding(10.dp))
+
+                            Column {
+                                Text(
+                                    text = "Swipe Layout",
+                                    color = Color.White,
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                                Spacer(modifier = Modifier.padding(10.dp))
+
+                                Text(
+                                    text = "Lorem Ipsum is simply dummy text of the printing and type setting industry...",
+                                    color = Color.White,
+                                    fontSize = 18.sp
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-
-
 }
-
-
-
-
-
-
-
-
 
 
 
