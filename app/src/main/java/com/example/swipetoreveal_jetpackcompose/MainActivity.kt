7addd8308787478fef982969e8d37797c8d57abf
mohamedtamer0 <mt4452242@gmail.com>
2022-01-13 05:24:11 +0200

    Update

--- app/src/main/java/com/example/swipetoreveal_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/swipetoreveal_jetpackcompose/MainActivity.kt
@@ -5,12 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.swipetoreveal_jetpackcompose.ui.theme.Purple200
+import com.example.swipetoreveal_jetpackcompose.ui.theme.Purple500
 import com.example.swipetoreveal_jetpackcompose.ui.theme.SwipeToRevealjetPackComposeTheme
 
 @ExperimentalMaterialApi
@@ -45,6 +56,52 @@ fun SwipeCompose() {
     val swipAbleState = rememberSwipeableState(initialValue = 0)
     val sizePx = with(LocalDensity.current) { squareSize.toPx() }
     val anchors = mapOf(0f to 0, sizePx to 1)
+
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Purple500)
+                .padding(15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Compose Swipe",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(15.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.LightGray)
+                .swipeable(
+                    state = swipAbleState,
+                    anchors = anchors,
+                    thresholds = { _, _ ->
+                        FractionalThreshold(0.3f)
+                    },
+                    orientation = Orientation.Horizontal
+                )
+            ) {
+                Column() {
+                    
+                }
+            }
+        }
+    }
 
 
 }
